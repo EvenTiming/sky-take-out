e32875d2a4a68d937215d7539f7c12985b2ac8bf
Monica <alextonywang@gmail.com>
2024-01-22 18:03:54 +0800

    启用禁用员工

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -89,4 +89,13 @@ public class EmployeeController {
         return Result.success(pageResult);
     }
 
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工帐号")
+    public Result startOrStop(@PathVariable Integer status, Long id){
+        log.info("启用禁用员工帐号 id :{}" ,id);
+        employeeService.startOrStop(status, id);
+        return Result.success();
+    }
+
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -102,4 +102,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         PageResult pageResult =  new PageResult(page.getTotal(), page.getResult());
         return pageResult;
     }
+
+    public void startOrStop(Integer status, Long id){
+        Employee employee = new Employee();
+        employee.setStatus(status);
+        employee.setId(id);
+        employeeMapper.update(employee);
+    }
+
 }
